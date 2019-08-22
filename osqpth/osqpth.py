ddb6f1026e8e724bc6a811ac5fb2c1afd45b85ac
Brandon Amos <brandon.amos.cs@gmail.com>
2019-08-21 17:55:02 -0700

    Split osqpth out into Module/Function, add some automatic batch detection.

--- osqpth/osqpth.py
+++ osqpth/osqpth.py
@@ -1,4 +1,5 @@
 import torch
+from torch.nn import Module
 from torch.autograd import Function
 import osqp
 import numpy as np
@@ -6,15 +7,11 @@ import scipy.sparse as spa
 import scipy.sparse.linalg as sla
 from .util import to_numpy
 
-
-class OSQP(Function):
-    def __init__(self,
-                 P_idx, P_shape,
-                 A_idx, A_shape,
-                 eps_rel=1e-05,
-                 eps_abs=1e-05,
-                 verbose=False,
+class OSQP(Module):
+    def __init__(self, P_idx, P_shape, A_idx, A_shape,
+                 eps_rel=1e-5, eps_abs=1e-5, verbose=False,
                  max_iter=10000):
+        super().__init__()
         self.eps_abs = eps_abs
         self.eps_rel = eps_rel
         self.verbose = verbose
@@ -22,9 +19,22 @@ class OSQP(Function):
         self.P_idx, self.P_shape = P_idx, P_shape
         self.A_idx, self.A_shape = A_idx, A_shape
 
-        # TODO: Perform OSQP Setup first to allocate memory?
-
     def forward(self, P_val, q_val, A_val, l_val, u_val):
+        return _OSQP.apply(
+            P_val, q_val, A_val, l_val, u_val,
+            self.P_idx, self.P_shape,
+            self.A_idx, self.A_shape,
+            self.eps_rel, self.eps_abs,
+            self.verbose, self.max_iter
+        )
+
+
+class _OSQP(Function):
+    @staticmethod
+    def forward(ctx, P_val, q_val, A_val, l_val, u_val,
+                A_idx, A_shape, P_idx, P_shape,
+                eps_rel=1e-5, eps_abs=1e-5,
+                verbose=False, max_iter=10000):
         """Solve a batch of QPs using OSQP.
 
         This function solves a batch of QPs, each optimizing over
@@ -64,9 +74,26 @@ class OSQP(Function):
 
         """
 
+        ctx.eps_abs = eps_abs
+        ctx.eps_rel = eps_rel
+        ctx.verbose = verbose
+        ctx.max_iter = max_iter
+        ctx.P_idx, ctx.P_shape = P_idx, P_shape
+        ctx.A_idx, ctx.A_shape = A_idx, A_shape
+
+        params = [P_val, q_val, A_val, l_val, u_val]
+
+        for p in params:
+            assert p.ndimension() <= 2, 'Unexpected number of dimensions'
+
         # Convert batches to sparse matrices/vectors
-        self.n_batch = P_val.size(0) if len(P_val.size()) > 1 else 1
-        self.m, self.n = self.A_shape   # Problem size
+        batch_mode = np.all([t.ndimension() == 1 for t in params])
+        if batch_mode:
+            ctx.n_batch = 1
+        else:
+            batch_sizes = [t.size(0) if t.ndimension() == 2 else 1 for t in params]
+            ctx.n_batch = max(batch_sizes)
+        ctx.m, ctx.n = ctx.A_shape   # Problem size
 
         dtype = P_val.dtype
         device = P_val.device
@@ -75,28 +102,29 @@ class OSQP(Function):
         # TODO (Bart): create CSC matrix during initialization. Then
         # just reassign the mat.data vector with A_val and P_val
 
-        if self.n_batch == 1:
-            # Create lists to make the code below work
-            # TODO (Bart): Find a better way to do this
-            P_val, q_val, A_val, l_val, u_val = [P_val], [q_val], [A_val], [l_val], [u_val]
+        for i, p in enumerate(params):
+            if p.ndimension() == 1:
+                params[i] = p.unsqueeze(0).expand(ctx.n_batch, p.size(0))
 
-        P = [spa.csc_matrix((to_numpy(P_val[i]), self.P_idx), shape=self.P_shape)
-             for i in range(self.n_batch)]
-        q = [to_numpy(q_val[i]) for i in range(self.n_batch)]
-        A = [spa.csc_matrix((to_numpy(A_val[i]), self.A_idx), shape=self.A_shape)
-             for i in range(self.n_batch)]
-        l = [to_numpy(l_val[i]) for i in range(self.n_batch)]
-        u = [to_numpy(u_val[i]) for i in range(self.n_batch)]
+        [P_val, q_val, A_val, l_val, u_val] = params
+
+        P = [spa.csc_matrix((to_numpy(P_val[i]), ctx.P_idx), shape=ctx.P_shape)
+             for i in range(ctx.n_batch)]
+        q = [to_numpy(q_val[i]) for i in range(ctx.n_batch)]
+        A = [spa.csc_matrix((to_numpy(A_val[i]), ctx.A_idx), shape=ctx.A_shape)
+             for i in range(ctx.n_batch)]
+        l = [to_numpy(l_val[i]) for i in range(ctx.n_batch)]
+        u = [to_numpy(u_val[i]) for i in range(ctx.n_batch)]
 
         # Perform forward step solving the QPs
-        x_torch = torch.zeros((self.n_batch, self.n), dtype=dtype, device=device)
+        x_torch = torch.zeros((ctx.n_batch, ctx.n), dtype=dtype, device=device)
 
         x, y, z = [], [], []
-        for i in range(self.n_batch):
+        for i in range(ctx.n_batch):
             # Solve QP
             # TODO: Cache solver object in between
             m = osqp.OSQP()
-            m.setup(P[i], q[i], A[i], l[i], u[i], verbose=self.verbose)
+            m.setup(P[i], q[i], A[i], l[i], u[i], verbose=ctx.verbose)
             result = m.solve()
             status = result.info.status
             if status != 'solved':
@@ -112,34 +140,40 @@ class OSQP(Function):
             x_torch[i] = torch.from_numpy(result.x)
 
         # Save stuff for backpropagation
-        self.backward_vars = (P, q, A, l, u, x, y, z)
+        ctx.backward_vars = (P, q, A, l, u, x, y, z)
 
         # Return solutions
+        if not batch_mode:
+            x_torch = x_torch.squeeze(0)
         return x_torch
 
-    def backward(self, dl_dx_val):
-
+    @staticmethod
+    def backward(ctx, dl_dx_val):
         dtype = dl_dx_val.dtype
         device = dl_dx_val.device
 
+        batch_mode = dl_dx_val.ndimension() == 2
+        if not batch_mode:
+            dl_dx_val = dl_dx_val.unsqueeze(0)
+
         # Convert dl_dx to numpy
-        dl_dx = to_numpy(dl_dx_val).squeeze()
+        dl_dx = to_numpy(dl_dx_val)
 
         # Extract data from forward pass
-        P, q, A, l, u, x, y, z = self.backward_vars
+        P, q, A, l, u, x, y, z = ctx.backward_vars
 
         # Convert to torch tensors
-        nnz_P = len(self.P_idx[0])
-        nnz_A = len(self.A_idx[0])
-        dP = torch.zeros((self.n_batch, nnz_P), dtype=dtype, device=device)
-        dq = torch.zeros((self.n_batch, self.n), dtype=dtype, device=device)
-        dA = torch.zeros((self.n_batch, nnz_A), dtype=dtype, device=device)
-        dl = torch.zeros((self.n_batch, self.m), dtype=dtype, device=device)
-        du = torch.zeros((self.n_batch, self.m), dtype=dtype, device=device)
+        nnz_P = len(ctx.P_idx[0])
+        nnz_A = len(ctx.A_idx[0])
+        dP = torch.zeros((ctx.n_batch, nnz_P), dtype=dtype, device=device)
+        dq = torch.zeros((ctx.n_batch, ctx.n), dtype=dtype, device=device)
+        dA = torch.zeros((ctx.n_batch, nnz_A), dtype=dtype, device=device)
+        dl = torch.zeros((ctx.n_batch, ctx.m), dtype=dtype, device=device)
+        du = torch.zeros((ctx.n_batch, ctx.m), dtype=dtype, device=device)
 
         # TODO: Improve this, reuse OSQP, port it in C
 
-        for i in range(self.n_batch):
+        for i in range(ctx.n_batch):
             # Construct linear system
             # Taken from https://github.com/oxfordcontrol/osqp-python/blob/0363d028b2321017049360d2eb3c0cf206028c43/modulepurepy/_osqp.py#L1717
             # Guess which linear constraints are lower-active, upper-active, free
@@ -154,33 +188,40 @@ class OSQP(Function):
             # Form KKT linear system
             KKT = spa.vstack([spa.hstack([P[i], A_red.T]),
                               spa.hstack([A_red, spa.csc_matrix((n_low + n_upp, n_low + n_upp))])])
-            rhs = np.hstack([dl_dx.squeeze(), np.zeros(n_low + n_upp)])
+            rhs = np.hstack([dl_dx[i], np.zeros(n_low + n_upp)])
 
             # Get solution
             r_sol = sla.spsolve(KKT, rhs)
-            r_x =  r_sol[:self.n]
-            r_yl = r_sol[self.n:self.n + n_low]
-            r_yu = r_sol[self.n + n_low:]
-            r_y = np.zeros(self.m)
+
+            r_x =  r_sol[:ctx.n]
+            r_yl = r_sol[ctx.n:ctx.n + n_low]
+            r_yu = r_sol[ctx.n + n_low:]
+            r_y = np.zeros(ctx.m)
             r_y[ind_low] = r_yl
             r_y[ind_upp] = r_yu
 
              # Extract derivatives
-            rows, cols = self.P_idx
+            rows, cols = ctx.P_idx
             values = -.5 * (r_x[rows] * x[i][cols] + r_x[cols] * x[i][rows])
             dP[i] = torch.from_numpy(values)
 
-            rows, cols = self.A_idx
+            rows, cols = ctx.A_idx
             values = -(y[i][rows] * r_x[cols] + r_y[rows] * x[i][cols])
             dA[i] = torch.from_numpy(values)
             dq[i] = torch.from_numpy(-r_x)
-            dl[i] = torch.tensor(
-                [r_yl[np.where(ind_low == j)[0]] if j in ind_low else 0
-                 for j in range(self.m)])
-            du[i] = torch.tensor(
-                [r_yu[np.where(ind_upp == j)[0]] if j in ind_upp else 0
-                 for j in range(self.m)])
+            t = np.hstack([r_yl[np.where(ind_low == j)[0]] if j in ind_low else 0
+                 for j in range(ctx.m)])
+            dl[i] = torch.tensor(t)
+            t = np.hstack([r_yu[np.where(ind_upp == j)[0]] if j in ind_upp else 0
+                 for j in range(ctx.m)])
+            du[i] = torch.tensor(t)
 
-        grads = (dP, dq, dA, dl, du)
+        grads = [dP, dq, dA, dl, du]
 
-        return grads
+        if not batch_mode:
+            for i, g in enumerate(grads):
+                grads[i] = g.squeeze()
+
+        grads += [None]*8
+
+        return tuple(grads)
